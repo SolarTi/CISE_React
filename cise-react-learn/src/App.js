42833d261d0420c24e1c635c0dac72e2500e6e6c
SolaireTi <79572077+SolarTi@users.noreply.github.com>
2022-03-11 12:55:28 +1300

    Change Link as AUT page in App.js

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-03-11 12:37:41
- * @LastEditTime: 2022-03-11 12:39:17
- * @LastEditors: your name
+ * @LastEditTime: 2022-03-11 12:50:35
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \CISE_React\cise-react-learn\src\App.js
  */
@@ -19,7 +19,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://aut.ac.nz "
           target="_blank"
           rel="noopener noreferrer"
         >
